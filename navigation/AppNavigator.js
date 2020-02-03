f8d6871621465ecab1170afebd4e8bbb68cc74c9
Anton Begehr <a.begehr@gmail.com>
2020-02-03 16:44:46 +0100

    remove tabs, add stack, updates

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import MainTabNavigator from './MainTabNavigator';
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 
 export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+  createStackNavigator(
+    {
+      // You could add another route here for authentication.
+      // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+      Home: HomeScreen,
+      Links: LinksScreen,
+      Settings: SettingsScreen
+    },
+    {
+      initialRouteName: "Home"
+    }
+  )
 );
