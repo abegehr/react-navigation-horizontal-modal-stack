14dbb93f3e2f8c4b803e17efd4acb05e7ce116a3
Anton Begehr <a.begehr@gmail.com>
2020-02-03 17:41:06 +0100

    add cradStyleInterpolator for horizontal modal
    https://github.com/react-navigation/stack/issues/394#issuecomment-581487909

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,57 @@
 import React from "react";
+import { Animated } from "react-native";
 import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import {
+  createStackNavigator,
+  TransitionPresets
+} from "react-navigation-stack";
 
 import HomeScreen from "../screens/HomeScreen";
 import LinksScreen from "../screens/LinksScreen";
 import SettingsScreen from "../screens/SettingsScreen";
+
+const { multiply } = Animated;
+
+export function forHorizontalModal({
+  current,
+  next,
+  inverted,
+  layouts: { screen }
+}: StackCardInterpolationProps): StackCardInterpolatedStyle {
+  const translateFocused = multiply(
+    current.progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [screen.width, 0],
+      extrapolate: "clamp"
+    }),
+    inverted
+  );
+
+  const overlayOpacity = current.progress.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 0.07],
+    extrapolate: "clamp"
+  });
+
+  const shadowOpacity = current.progress.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 0.3],
+    extrapolate: "clamp"
+  });
+
+  return {
+    cardStyle: {
+      transform: [
+        // Translation for the animation of the current card
+        { translateX: translateFocused },
+        // Translation for the animation of the card in back
+        { translateX: 0 }
+      ]
+    },
+    overlayStyle: { opacity: overlayOpacity },
+    shadowStyle: { shadowOpacity }
+  };
+}
 
 export default createAppContainer(
   createStackNavigator(
@@ -12,11 +59,17 @@ export default createAppContainer(
       Home: HomeScreen,
       Links: {
         screen: LinksScreen,
-        navigationOptions: { gestureDirection: "horizontal" }
+        navigationOptions: {
+          gestureDirection: "horizontal",
+          cardStyleInterpolator: forHorizontalModal
+        }
       },
       Settings: {
         screen: SettingsScreen,
-        navigationOptions: { gestureDirection: "horizontal-inverted" }
+        navigationOptions: {
+          gestureDirection: "horizontal-inverted",
+          cardStyleInterpolator: forHorizontalModal
+        }
       }
     },
     {
